13471ee5b7f6cf45372cd204f1eb0978c23c8213
Natalie Agus <nn88blue@gmail.com>
2024-11-27 12:59:47 +0800

    feat: finish w11 l2

--- cohort-problems/playground.py
+++ cohort-problems/playground.py
@@ -1,19 +1,8 @@
-import numpy as np 
+import numpy
+import itertools 
 
-# 1
-# 2
-# 3
-vector = [[1],[2],[3]]
+labels = [0,1]
 
-"""
-1 1 1 1
-4 4 4 4 
-8 8 8 8
-"""
-array = np.array([[1,1,1,1], [4,4,4,4], [8,8,8,8]]) 
-
-
-mean_axis0 = array.mean(axis=0) # 1st column mean = (1+4+8) / 3, etc, we end up with 4 means, one for each column 
-mean_axis1 = array.mean(axis=1) # 1st row mean = (1+1+1+1) / 4, we end up with 3 means, one for each row 
-print("mean_axis0", mean_axis0)
-print("mean_axis1", mean_axis1)
+keys = itertools.product(labels, repeat=3)
+for k in keys: 
+    print(k)
